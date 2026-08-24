f1d6b9fa93f8392cc70e121e619917e2d195ded4
HASSAN SOHRAT <hassansohrat@outlook.com>
2026-08-24 12:21:21 +0600

    Half baked tree and nodes.

--- src/Main.java
+++ src/Main.java
@@ -28,8 +28,80 @@ public class Node{
     public Node[] getChildren(){ return children; }
 };
 
+public class Queue{
+    int capacity;
+    int size;
+    Node[] mainQ;
+    Node front;
+    Node rear;
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Node[] getMainQ() {
+        return mainQ;
+    }
+
+    public Node getFront() {
+        return front;
+    }
+
+    public Node getRear() {
+        return rear;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void setMainQ(Node[] mainQ) {
+        this.mainQ = mainQ;
+    }
+
+    public void setFront(Node front) {
+        this.front = front;
+    }
+
+    public void setRear(Node rear) {
+        this.rear = rear;
+    }
+
+    boolean isEmpty(){}
+
+    boolean isFull(){}
+
+    void enqueue(Node newNode){}
+
+    void dequeue(){}
+};
+
 public class Tree{
     private Node root;
+
+    public Tree(Node root){
+        this.root = root;
+    }
+
+    public void setRoot(Node root){ this.root = root; }
+
+    public Node getRoot(){ return root; }
+
+    // public Node searchTravarse(){
+    //     Node result;
+    //     int[] queue;
+    //     int[] visited;
+
+    //     whileif(!(queue == null)){
+            
+    //     }
+
+    //     return result;
+    // }
 }
 
 void main(){
